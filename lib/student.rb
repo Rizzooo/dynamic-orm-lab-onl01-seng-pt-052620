a297f0b16bbc16bdc7867a6fe5c6243d4a4945ea
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-30 15:40:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,10 +6,4 @@ class Student < InteractiveRecord
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
-  
-  def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
-    
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
 end
